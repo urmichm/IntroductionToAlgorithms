1dd3f25a416eac70213c8d5ecf3ba606d42fd4a2
Mike Urmich <urmich.m@gmail.com>
2020-12-18 23:48:27 +0100

    Inversion Count
    DnC chapter
    Inversions Count problem added
    
    yes, yes, function-definitions will be moved to .c file
    when? soon

--- Code/main.cpp
+++ Code/main.cpp
@@ -3,26 +3,24 @@
 #include <vector>
 #include <iostream>
 
-#include "MergeSort.h"
+#include "CountInversions.h"
 #include "MaximumSubarray.h"
 
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	vector<int> arr({5,4,1,8,7,2,6,3,9});
+	vector<int> arr({7,1,3,5,2,4,6});
 	cout << "Vector created" << endl;
 
-	MergeSort(arr);
-	cout << "MergeSorte:" << endl;
+	cout << "Inversions: " << CountInversions(arr) << endl;
 	for (int i = 0; i < arr.size(); i++)
 		cout << arr[i] << " ";
 	cout << endl;
 
 
-	vector<int> nums({ -2,1,-3,4,-1,2,1,-5,4 });
-
-	cout << "Expected max subarray is 6, actual is " << MaxSubArray(nums) << endl;
+	//vector<int> nums({ -2,1,-3,4,-1,2,1,-5,4 });
+	//cout << "Expected max subarray is 6, actual is " << MaxSubArray(nums) << endl;
 
 	cout << endl << endl;
 	return 0;
