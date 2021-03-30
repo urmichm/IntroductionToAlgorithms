79f9efb0cd82ee068c67fcd422d03fbfd3657ac8
Mike Urmich <urmich.m@gmail.com>
2021-03-30 16:32:29 +0200

    Find the Town Judge problem added
    Find the Town Judge problem added
    LeetCode 997.

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,11 +1,14 @@
 #define _CRTDBG_MAP_ALLOC
-#include "3sum.h"
 #include <crtdbg.h>
 using namespace std;
 
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	
+#ifdef FIND_THE_TOWN_JUDGE
+		run();
+#endif // FIND_THE_TOWN_JUDGE
 
 #ifdef THREE_SUM_PROBLEM
 	run();
